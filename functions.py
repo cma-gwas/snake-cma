cbc31dd5e74bea06a8d34fcd56628b4d9be0140e
iozkarac <iozkarac@exseed.ed.ac.uk>
2023-02-23 14:24:24 +0000

    Meta-Analysis v.0.1.. Simplified and Optimised for post meta-analysis..

--- functions.py
+++ functions.py
@@ -3,38 +3,45 @@ import numpy as np
 from math import sqrt
 from scipy.stats import chi2
 
-def import_results(path_prefix_, split_index_, cols_=['A1FREQ','BETA', 'SE'], \
-cols__=['CHROM','GENPOS','ID','ALLELE0','ALLELE1']):
-    if split_index_ == 1:
-        df_temp = pd.read_csv(path_prefix_ % 1, usecols= cols__ + cols_, sep=" ", header=0)
-        return (np.array(df_temp['BETA'], dtype=np.float32).reshape((-1,1,1)),\
-np.array(df_temp['SE'], dtype=np.float32).reshape((-1,1,1)), df_temp[cols__+['A1FREQ']])
-    else:
-        df_temp = pd.read_csv(path_prefix_ % 1, usecols= cols__ + cols_, sep=" ", header=0)
-        beta_ = np.array(df_temp['BETA'], dtype=np.float32).reshape((-1,1,1))
-        se_ = np.array(df_temp['SE'], dtype=np.float32).reshape((-1,1,1))
-        n_ = 2 * len(df_temp)
-        f_ = np.array(df_temp['A1FREQ'], dtype=np.float32) * n_
-        for i in range(2,split_index_+1):
-            df_temp_2 = pd.read_csv(path_prefix_ % i, usecols= cols_, sep=" ", header=0)
-            beta_ = np.hstack((beta_,np.array(df_temp_2['BETA'], dtype=np.float32).reshape((-1,1,1))))
-            se_ = np.hstack((se_,np.array(df_temp_2['SE'], dtype=np.float32).reshape((-1,1,1))))
-            f_ = f_ + np.array(df_temp_2['A1FREQ'],dtype=np.float32) * 2 * len(df_temp_2)
-            n_ = n_ + 2 * len(df_temp_2)
-        df_temp['A1FREQ'] = f_/n_
-        df_temp['A1FREQ'] = np.round(np.array(df_temp['A1FREQ']),6)
-        return (beta_,se_,df_temp[cols__+['A1FREQ']])
+def check_inputs():
+    assert args.split is not None, "Split Index is required.."
+    assert args.meta_summary or args.meta_no_correction, "Choose a flag for meta-analysis procedure. Currently avaiable options are: --meta_summary and --meta_no_correction."
+    assert args.out is not None, "Output path is required.."
+
+def initial_logging(log, inputs, inputs_Null, output):
+    check_inputs()
+    with open(output, "w") as f:
+        f.write(log % (datetime.datetime.now().strftime("%A - %d %B %Y - %H:%M:%S"), str(os.getcwd())))
+        for i in inputs:
+            if inputs[i]!= None and type(inputs[i])!=bool:
+                f.write("\t--%s %s\n" % (str(i), str(inputs[i])))
+            elif inputs[i]!=inputs_Null[i] and type(inputs[i])==bool:
+                f.write("\t--%s \n" % str(i))
+
+def import_results(path_prefix_, split_index_, cols_=['A1FREQ','BETA', 'SE'], cols__=['CHROM','GENPOS','ID','ALLELE0','ALLELE1']):
+    # split_index_ > 1
+    df_temp = pd.read_csv(path_prefix_ % 1, usecols= cols__ + cols_, sep=" ", header=0)
+    beta_ = np.array(df_temp['BETA'], dtype=np.float32).reshape((-1,1,1))
+    se_ = np.array(df_temp['SE'], dtype=np.float32).reshape((-1,1,1))
+    n_ = 2 * len(df_temp)
+    f_ = np.array(df_temp['A1FREQ'], dtype=np.float32) * n_
+    for i in range(2,split_index_+1):
+        df_temp_2 = pd.read_csv(path_prefix_ % i, usecols= cols_, sep=" ", header=0)
+        beta_ = np.hstack((beta_,np.array(df_temp_2['BETA'], dtype=np.float32).reshape((-1,1,1))))
+        se_ = np.hstack((se_,np.array(df_temp_2['SE'], dtype=np.float32).reshape((-1,1,1))))
+        f_ = f_ + np.array(df_temp_2['A1FREQ'],dtype=np.float32) * 2 * len(df_temp_2)
+        n_ = n_ + 2 * len(df_temp_2)
+    df_temp['A1FREQ'] = f_/n_
+    df_temp['A1FREQ'] = np.round(np.array(df_temp['A1FREQ']),6)
+    return (beta_,se_,df_temp[cols__+['A1FREQ']])
 
 def compute_cor_summary(beta_,se_,correction_bool=True):
     z_ = beta_ / se_
-    alpha_ = np.array([[(z_[:,i,0] * z_[:,j,0])/np.abs(z_[:,i,0] * z_[:,j,0]) \
-for i in range(len(z_[0]))] for j in range(len(z_[0]))]).transpose(2,0,1)
+    alpha_ = np.array([[(z_[:,i,0] * z_[:,j,0])/np.abs(z_[:,i,0] * z_[:,j,0]) for i in range(len(z_[0]))] for j in range(len(z_[0]))]).transpose(2,0,1)
     if correction_bool:
-        return alpha_ * np.repeat(np.array([[round(np.corrcoef(z_[:,i,0],z_[:,j,0])[0,1],13) \
-for i in range(len(z_[0]))] for j in range(len(z_[0]))], dtype=np.float32)[np.newaxis, :, :], len(se_), axis=0)
+        return alpha_ * np.repeat(np.array([[round(np.corrcoef(z_[:,i,0],z_[:,j,0])[0,1],13) for i in range(len(z_[0]))] for j in range(len(z_[0]))], dtype=np.float32)[np.newaxis, :, :], len(se_), axis=0)
     else:
-        return np.repeat(np.array([[round(np.corrcoef(z_[:,i,0],z_[:,j,0])[0,1],13) \
-for i in range(len(z_[0]))] for j in range(len(z_[0]))], dtype=np.float32)[np.newaxis, :, :], len(se_), axis=0)
+        return np.repeat(np.array([[round(np.corrcoef(z_[:,i,0],z_[:,j,0])[0,1],13) for i in range(len(z_[0]))] for j in range(len(z_[0]))], dtype=np.float32)[np.newaxis, :, :], len(se_), axis=0)
 
 def compute_cov(corr_matrix, se_):
     return se_ * corr_matrix * se_.transpose(0,2,1) # returns M x K x K numpy array
@@ -48,6 +55,7 @@ def compute_weights(cov_matrix, se_, split_index_):
     e_temp_2 =  np.repeat(np.ones(split_index_, dtype=np.float32).reshape((1,split_index_))[np.newaxis, :, :], len(se_), axis=0)
     M = len(se_)
 
+    ## We compute M many inverse of Cov Matrices (K x K). Because these are independent, we have split this step into two for resource efficiency purposes..
     inv_cov_matrix_1 = np.linalg.inv(cov_matrix[:int(len(se_)/2)]) # returns M x K x K numpy array
     inv_cov_matrix_2 = np.linalg.inv(cov_matrix[int(len(se_)/2):]) # returns M x K x K numpy array
     inv_cov_matrix= np.concatenate([inv_cov_matrix_1,inv_cov_matrix_2], axis=0)
@@ -60,31 +68,48 @@ def compute_stats(weights, cov_matrix, beta_, se_, split_index_, df_):
     df_['SE'] = np.round(se_final_,6)
     df_['CHISQ'] = (np.array(df_['BETA'])/np.array(df_['SE']))**2
     df_['LOG10P'] = -1 * np.log10(chi2.sf(df_['CHISQ'], 1, loc=0, scale=1))
+    df_['P'] = -1 * np.log10(chi2.sf(df_['CHISQ'], 1, loc=0, scale=1))
     df_ = df_.set_index('CHROM')
     df_['CHISQ'] = df_['CHISQ'].apply('{:,.3e}'.format)
+    df_ = df_[['GENPOS', 'ID', 'ALLELE0', 'ALLELE1', 'A1FREQ', 'BETA', 'SE', 'CHISQ', 'LOG10P', 'P']]
+    print('Done..')
     return df_
 
 def meta_nocorrection(path_prefix_, split_index_):
-    beta_,se_,df_ = import_results(path_prefix_, split_index_)
-    M = sum(1 for line in open(path_prefix_ % 1)) - 1
-    corr_matrix_ = np.repeat(np.identity(split_index_)[np.newaxis,:,:],M,axis=0)
-    del M
-    cov_matrix_ = compute_cov(corr_matrix_, se_)
-    del corr_matrix_
-    weights_ = compute_weights(cov_matrix_, se_, split_index_)
-    return compute_stats(weights_, cov_matrix_, beta_, se_, split_index_, df_)
+    if split_index_ == 1:
+        df_ = pd.read_csv(path_prefix_ % 1, usecols= ['CHROM','GENPOS', 'ID', 'ALLELE0', 'ALLELE1', 'A1FREQ', 'BETA', 'SE', 'CHISQ', 'LOG10P'], sep=" ", header=0)
+        df_ = df_.set_index('CHROM')
+        df_['P'] = -1 * np.log10(chi2.sf(df_['CHISQ'], 1, loc=0, scale=1))
+        return df_
+    else:
+        beta_,se_,df_ = import_results(path_prefix_, split_index_)
+        print('Results are imported..')
+        M = sum(1 for line in open(path_prefix_ % 1)) - 1
+        corr_matrix_ = np.repeat(np.identity(split_index_)[np.newaxis,:,:],M,axis=0)
+        print('Correlation Matrix is computed..')
+        cov_matrix_ = compute_cov(corr_matrix_, se_)
+        print('Covariance Matrix is computed..')
+        del corr_matrix_, M
+        weights_ = compute_weights(cov_matrix_, se_, split_index_)
+        print('Weights computed..')
+        return compute_stats(weights_, cov_matrix_, beta_, se_, split_index_, df_)
 
 def meta_summary(path_prefix_, split_index_):
-    beta_,se_,df_ = import_results(path_prefix_, split_index_)
-    print('Results are imported..')
-    corr_matrix_ = compute_cor_summary(beta_,se_)
-    print('Correlation Matrix is computed..')
-    cov_matrix_ = compute_cov(corr_matrix_, se_)
-    print('Covariance Matrix is computed..')
-    del corr_matrix_
-    weights_ = compute_weights(cov_matrix_, se_, split_index_)
-    print('Weights computed..')
-    return compute_stats(weights_, cov_matrix_, beta_, se_, split_index_, df_)
-
+    if split_index_ == 1:
+        df_ = pd.read_csv(path_prefix_ % 1, usecols= ['CHROM','GENPOS', 'ID', 'ALLELE0', 'ALLELE1', 'A1FREQ', 'BETA', 'SE', 'CHISQ', 'LOG10P'], sep=" ", header=0)
+        df_ = df_.set_index('CHROM')
+        df_['P'] = -1 * np.log10(chi2.sf(df_['CHISQ'], 1, loc=0, scale=1))
+        return df_
+    else:
+        beta_,se_,df_ = import_results(path_prefix_, split_index_)
+        print('Results are imported..')
+        corr_matrix_ = compute_cor_summary(beta_,se_)
+        print('Correlation Matrix is computed..')
+        cov_matrix_ = compute_cov(corr_matrix_, se_)
+        print('Covariance Matrix is computed..')
+        del corr_matrix_
+        weights_ = compute_weights(cov_matrix_, se_, split_index_)
+        print('Weights computed..')
+        return compute_stats(weights_, cov_matrix_, beta_, se_, split_index_, df_)
 
 
